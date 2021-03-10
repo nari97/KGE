1ca7dbb7da1502d288109a90b902ad1f80d6c7f6
nari97 <narayananak12@gmail.com>
2021-03-10 11:39:12 -0500

    Removed divison

--- Loss/SigmoidLoss.py
+++ Loss/SigmoidLoss.py
@@ -23,7 +23,7 @@ class SigmoidLoss(Loss):
             return -(self.criterion(p_score).mean() + (self.get_weights(n_score) * self.criterion(-n_score)).sum(
                 dim=-1).mean()) / 2
         else:
-             return -(self.criterion(p_score).mean() + self.criterion(-n_score).mean()) / 2
+             return -(self.criterion(p_score).mean() + self.criterion(-n_score).mean())
 
     def predict(self, p_score, n_score):
         score = self.forward(p_score, n_score)
