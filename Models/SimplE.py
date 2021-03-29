b19d0becd1090d071c1b8a009960e233d305e503
nari97 <narayananak12@gmail.com>
2021-03-29 13:07:38 -0400

    Added SimplE

--- Models/SimplE.py
+++ Models/SimplE.py
@@ -8,11 +8,13 @@ class SimplE(Model):
         super(SimplE, self).__init__(ent_tot, rel_tot)
 
         self.dim = dim
-        self.ent_embeddings = nn.Embedding(self.ent_tot, self.dim)
+        self.ent_h_embeddings = nn.Embedding(self.ent_tot, self.dim)
+        self.ent_t_embeddings = nn.Embedding(self.ent_tot, self.dim)
         self.rel_embeddings = nn.Embedding(self.rel_tot, self.dim)
         self.rel_inv_embeddings = nn.Embedding(self.rel_tot, self.dim)
 
-        nn.init.xavier_uniform_(self.ent_embeddings.weight.data)
+        nn.init.xavier_uniform_(self.ent_h_embeddings.weight.data)
+        nn.init.xavier_uniform_(self.ent_t_embeddings.weight.data)
         nn.init.xavier_uniform_(self.rel_embeddings.weight.data)
         nn.init.xavier_uniform_(self.rel_inv_embeddings.weight.data)
 
@@ -30,12 +32,12 @@ class SimplE(Model):
         batch_h = data['batch_h']
         batch_t = data['batch_t']
         batch_r = data['batch_r']
-        h = self.ent_embeddings(batch_h)
-        t = self.ent_embeddings(batch_t)
+        h = self.ent_h_embeddings(batch_h)
+        t = self.ent_t_embeddings(batch_t)
         r = self.rel_embeddings(batch_r)
         r_inv = self.rel_inv_embeddings(batch_r)
         score = self._calc_avg(h, t, r, r_inv)
-        return score
+        return torch.clamp(score,-20, 20)
 
     def regularization(self, data):
         batch_h = data['batch_h']
@@ -52,8 +54,8 @@ class SimplE(Model):
         batch_h = data['batch_h']
         batch_t = data['batch_t']
         batch_r = data['batch_r']
-        h = self.ent_embeddings(batch_h)
-        t = self.ent_embeddings(batch_t)
+        h = self.ent_h_embeddings(batch_h)
+        t = self.ent_t_embeddings(batch_t)
         r = self.rel_embeddings(batch_r)
         score = -self._calc_ingr(h, r, t)
-        return score.cpu().data.numpy()
+        return torch.clamp(score,-20, 20)
